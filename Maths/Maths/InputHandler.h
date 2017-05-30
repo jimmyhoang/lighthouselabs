60f7ccb84601ee43fbaa1cd3e0770e774ec66481
Jimmy Hoang <hoangjimmy94@gmail.com>
2017-05-30 12:30:28 -0700

    Modify quit and add InputHandler

---
+++ Maths/Maths/InputHandler.h
@@ -0,0 +1,15 @@
+//
+//  InputHandler.h
+//  Maths
+//
+//  Created by Jimmy Hoang on 2017-05-30.
+//  Copyright © 2017 Jimmy Hoang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface InputHandler : NSObject
+
+-(NSString*)input;
+
+@end
--- Maths/Maths/main.m
+++ Maths/Maths/main.m
@@ -8,15 +8,15 @@
 
 #import <Foundation/Foundation.h>
 #import "AdditionQuestion.h"
+#import "InputHandler.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         BOOL gameOn = YES;
-        char userAnswer[255];
-       
         
         while (gameOn) {
             AdditionQuestion* question = [[AdditionQuestion alloc] init];
+            InputHandler* input = [[InputHandler alloc] init];
             
             // Output question to User
             NSLog(@"%@",question.question);
@@ -24,36 +24,28 @@ int main(int argc, const char * argv[]) {
             // Prompt user for answer
             NSLog(@"Enter your answer or type quit: ");
             
-            fgets(userAnswer, 255, stdin);
+            // Get user input
+            NSString* answer = [input input];
+
+            NSRange checkString = [answer rangeOfString:@"quit" options: NSCaseInsensitiveSearch];
             
-            // Convert C String to NSString
-            NSString *convertAnswer = [NSString stringWithCString:userAnswer encoding: NSUTF8StringEncoding];
-           
-            // To delete leading spaces and new line
-            NSCharacterSet *spaces = [NSCharacterSet whitespaceCharacterSet];
-            
-            // Remove leading spaces and new line from NSString
-            convertAnswer = [convertAnswer stringByTrimmingCharactersInSet:spaces];
-            
-            if (![convertAnswer isEqualToString:@"quit"]) {
+            // Check if user input "quit" or proper answer
+            if (checkString.location == 0) {
                 gameOn = NO;
                 continue;
             } else {
                 // Convert NSString answer to NSInteger
-                NSInteger userAnswer = [convertAnswer intValue];
+                NSInteger convertAnswer = [answer intValue];
                 
                 // Check if user answer is correct
-                    if (userAnswer == question.answer) {
+                    if (convertAnswer == question.answer) {
                         NSLog(@"Right!");
                     } else {
                         NSLog(@"Wrong!");
                     }
                 
             }
-            
-            
-            
-            
+
             
         }
     }
